46ac64fdc0a39bd1d357eafdd537b30274d45160
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-11-04 16:31:07 -0500

    [Tests] starts ReverseRegistrar tests

--- test/registry/FIFSRegistrar.test.ts
+++ test/registry/FIFSRegistrar.test.ts
@@ -1,68 +1,67 @@
-import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers'
-import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
-import namehash from 'eth-ens-namehash'
-import utils from 'web3-utils'
-import { endianness } from 'os'
-const sha3 = utils.sha3
+import namehash from 'eth-ens-namehash';
+import utils from 'web3-utils';
+const sha3 = utils.sha3;
 
 const ZERO_HASH =
-  '0x0000000000000000000000000000000000000000000000000000000000000000'
+  '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 const ARBITRARY_HASH =
-  '0x4f5b812789fc606be1b3b16908db13fc7a9adf7ca72641f84d75b47069d3d7f0'
+  '0x4f5b812789fc606be1b3b16908db13fc7a9adf7ca72641f84d75b47069d3d7f0';
 
 describe('FIFSRegistrar.sol', async () => {
   const deployFIFSRegistrar = async () => {
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners()
-    const BNS = await ethers.getContractFactory('BNSRegistry')
-    const bns = await BNS.deploy()
+    const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+    const BNS = await ethers.getContractFactory('BNSRegistry');
+    const bns = await BNS.deploy();
 
-    const FIFSRegistrar = await ethers.getContractFactory('FIFSRegistrar')
-    const registrar = await FIFSRegistrar.deploy(bns.address, ZERO_HASH)
+    const FIFSRegistrar = await ethers.getContractFactory('FIFSRegistrar');
+    const registrar = await FIFSRegistrar.deploy(bns.address, ZERO_HASH);
 
-    await bns.setOwner(ZERO_HASH, registrar.address)
+    await bns.setOwner(ZERO_HASH, registrar.address);
 
-    return { bns, registrar, owner, addr1, addr2, addr3 }
-  }
+    return { bns, registrar, owner, addr1, addr2, addr3 };
+  };
 
   it('should allow registration of names', async () => {
-    const { bns, registrar, owner } = await loadFixture(deployFIFSRegistrar)
+    const { bns, registrar, owner } = await loadFixture(deployFIFSRegistrar);
 
     expect(await registrar.register(sha3('b'), owner.address))
       .to.emit(bns, 'NewOwner')
-      .withArgs(ZERO_HASH, sha3('b'), owner.address)
+      .withArgs(ZERO_HASH, sha3('b'), owner.address);
 
-    expect(await bns.owner(ZERO_HASH)).to.eql(registrar.address)
+    expect(await bns.owner(ZERO_HASH)).to.eql(registrar.address);
 
-    expect(await bns.owner(namehash.hash('b'))).to.eql(owner.address)
-  })
+    expect(await bns.owner(namehash.hash('b'))).to.eql(owner.address);
+  });
 
   context('transferring names', async () => {
     it('should allow transferring name to your own', async () => {
       const { bns, registrar, owner, addr1 } = await loadFixture(
         deployFIFSRegistrar,
-      )
+      );
 
-      await registrar.register(sha3('b'), owner.address)
+      await registrar.register(sha3('b'), owner.address);
 
       expect(await registrar.register(sha3('b'), addr1.address))
         .to.emit(bns, 'NewOwner')
-        .withArgs(ZERO_HASH, sha3('b'), addr1.address)
+        .withArgs(ZERO_HASH, sha3('b'), addr1.address);
 
-      expect(await bns.owner(namehash.hash('b'))).to.eql(addr1.address)
-    })
+      expect(await bns.owner(namehash.hash('b'))).to.eql(addr1.address);
+    });
 
     it('forbids transferring the name you do not own', async () => {
       const { bns, registrar, owner, addr1 } = await loadFixture(
         deployFIFSRegistrar,
-      )
+      );
 
-      await registrar.register(sha3('b'), owner.address)
+      await registrar.register(sha3('b'), owner.address);
 
       await expect(registrar.connect(addr1).register(sha3('b'), addr1.address))
-        .to.be.reverted
-    })
-  })
-})
+        .to.be.reverted;
+    });
+  });
+});
--- test/registry/ReverseRegistrar.test.ts
+++ test/registry/ReverseRegistrar.test.ts
@@ -1,32 +1,120 @@
-import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers'
-import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
 
-import namehash from 'eth-ens-namehash'
-import utils from 'web3-utils'
-const sha3 = utils.sha3
+import namehash from 'eth-ens-namehash';
+import utils from 'web3-utils';
+import { BADHINTS } from 'dns';
+const sha3 = utils.sha3;
 
 function getReverseNode(addr: string): string {
-  return namehash.hash(addr.slice(2).toLowerCase() + '.addr.reverse')
+  return namehash.hash(addr.slice(2).toLowerCase() + '.addr.reverse');
 }
 
+const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
 const ZERO_HASH =
-  '0x0000000000000000000000000000000000000000000000000000000000000000'
-
-const ARBITRARY_HASH =
-  '0x4f5b812789fc606be1b3b16908db13fc7a9adf7ca72641f84d75b47069d3d7f0'
+  '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 describe('ReverseRegistrar.sol', async () => {
   const deployReverseRegistrar = async () => {
-    const [owner, addr1, addr2, addr3] = await ethers.getSigners()
-    const BNS = await ethers.getContractFactory('BNSRegistry')
-    const bns = await BNS.deploy()
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const node = getReverseNode(owner.address);
+    const node2 = getReverseNode(addr1.address);
+    const node3 = getReverseNode(addr2.address);
 
-    const FIFSRegistrar = await ethers.getContractFactory('FIFSRegistrar')
-    const registrar = await FIFSRegistrar.deploy(bns.address, ZERO_HASH)
+    const BNS = await ethers.getContractFactory('BNSRegistry');
+    const bns = await BNS.deploy();
 
-    await bns.setOwner(ZERO_HASH, registrar.address)
+    const NameWrapper = await ethers.getContractFactory('DummyNameWrapper');
+    const nameWrapper = await NameWrapper.deploy();
 
-    return { bns, registrar, owner, addr1, addr2, addr3 }
-  }
-})
+    const ReverseRegistrar = await ethers.getContractFactory(
+      'ReverseRegistrar',
+    );
+    const registrar = await ReverseRegistrar.deploy(bns.address);
+
+    const PublicResolver = await ethers.getContractFactory('PublicResolver');
+    const resolver = await PublicResolver.deploy(
+      bns.address,
+      nameWrapper.address,
+      '0x0000000000000000000000000000000000000000',
+      registrar.address,
+    );
+    await registrar.setDefaultResolver(resolver.address);
+
+    await bns.setSubnodeOwner(ZERO_HASH, sha3('reverse'), owner.address);
+    await bns.setSubnodeOwner(
+      namehash.hash('reverse'),
+      sha3('addr'),
+      registrar.address,
+    );
+
+    return {
+      bns,
+      registrar,
+      resolver,
+      owner,
+      node,
+      node2,
+      node3,
+      addr1,
+      addr2,
+    };
+  };
+
+  it('should calculate node hash correctly', async () => {
+    const { registrar, owner, node } = await loadFixture(
+      deployReverseRegistrar,
+    );
+    expect(await registrar.node(owner.address)).to.eql(node);
+  });
+
+  context('claim', async () => {
+    it('allows an account to claim its address', async () => {
+      const { bns, registrar, addr1, node } = await loadFixture(
+        deployReverseRegistrar,
+      );
+      registrar.claim(addr1.address);
+      expect(await bns.owner(node)).to.eql(addr1.address);
+    });
+
+    it('event ReveseClaimed is emitted0', async () => {
+      const { registrar, addr1, node } = await loadFixture(
+        deployReverseRegistrar,
+      );
+      expect(await registrar.claim(addr1.address))
+        .to.emit(registrar, 'ReverseClaimed')
+        .withArgs(addr1.address, node);
+    });
+  });
+
+  context('claimForAddr', async () => {
+    it('allows an account to claim its address', async () => {
+      const { bns, resolver, registrar, owner, addr1, node } =
+        await loadFixture(deployReverseRegistrar);
+
+      await registrar.claimForAddr(
+        owner.address,
+        addr1.address,
+        resolver.address,
+      );
+
+      expect(await bns.owner(node)).to.eql(addr1.address);
+    });
+
+    it('event ReverseClaimed is emitted', async () => {
+      const { bns, resolver, registrar, owner, addr1, node } =
+        await loadFixture(deployReverseRegistrar);
+
+      expect(
+        await registrar.claimForAddr(
+          owner.address,
+          addr1.address,
+          resolver.address,
+        ),
+      )
+        .to.emit(registrar, 'ReverseClaimed')
+        .withArgs(addr1.address, node);
+    });
+  });
+});
