f4a6c110f329fd776b075bf5401f7b5c7b0efe5c
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-11-08 10:49:23 -0600

    [Tests] BNSRegistrar tests finished

--- test/registry/BNSRegistrar.test.ts
+++ test/registry/BNSRegistrar.test.ts
@@ -47,4 +47,79 @@ describe('BNSRegistrar.sol', () => {
     expect(await bns.owner(namehash.hash('testname.b'))).to.eql(ZERO_ADDRESS);
     expect(await registrar.ownerOf(sha3('testname'))).to.eql(addr1.address);
   });
+
+  it('forbids registration of a name already registered', async () => {
+    const { bns, registrar, owner, addr1, addr3 } = await loadFixture(
+      deployBNSRegistrar,
+    );
+    await registrar.register(sha3('newname'), addr3.address);
+    await expect(registrar.register(sha3('newname'), addr1.address)).to.be
+      .reverted;
+    expect(await registrar.ownerOf(sha3('newname'))).to.eql(addr3.address);
+  });
+
+  it('should permit owner to reclaim a name', async () => {
+    const { bns, registrar, owner, addr2, addr3 } = await loadFixture(
+      deployBNSRegistrar,
+    );
+    await registrar.register(sha3('newname'), addr2.address);
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(addr2.address);
+    await bns.setSubnodeOwner(ZERO_HASH, sha3('b'), owner.address);
+    await bns.setSubnodeOwner(
+      namehash.hash('b'),
+      sha3('newname'),
+      ZERO_ADDRESS,
+    );
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(ZERO_ADDRESS);
+
+    await bns.setSubnodeOwner(ZERO_HASH, sha3('b'), registrar.address);
+    await registrar.connect(addr2).reclaim(sha3('newname'), addr2.address);
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(addr2.address);
+  });
+
+  it('forbids anyone else reclaiming a name', async () => {
+    const { bns, registrar, owner, addr2, addr3 } = await loadFixture(
+      deployBNSRegistrar,
+    );
+    await registrar.register(sha3('newname'), addr2.address);
+    await bns.setSubnodeOwner(ZERO_HASH, sha3('b'), owner.address);
+    await bns.setSubnodeOwner(
+      namehash.hash('b'),
+      sha3('newname'),
+      ZERO_ADDRESS,
+    );
+    await bns.setSubnodeOwner(ZERO_HASH, sha3('b'), registrar.address);
+    await expect(
+      registrar.connect(addr3).reclaim(sha3('newname'), addr3.address),
+    ).to.be.reverted;
+  });
+
+  it('should permit owner to transfer a registration', async () => {
+    const { bns, registrar, owner, addr1, addr3 } = await loadFixture(
+      deployBNSRegistrar,
+    );
+    await registrar.register(sha3('newname'), addr1.address);
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(addr1.address);
+    await registrar
+      .connect(addr1)
+      .transferFrom(addr1.address, addr3.address, sha3('newname'));
+    expect(await registrar.ownerOf(sha3('newname'))).to.eql(addr3.address);
+
+    //Transfer does now update BNS Registry without reclaim
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(addr1.address);
+    await registrar.connect(addr3).reclaim(sha3('newname'), addr3.address);
+    expect(await bns.owner(namehash.hash('newname.b'))).to.eql(addr3.address);
+  });
+
+  it('forbids non-onwer from transfering', async () => {
+    const { bns, registrar, owner, addr1, addr3 } = await loadFixture(
+      deployBNSRegistrar,
+    );
+    await registrar.register(sha3('newname'), addr1.address);
+    await expect(
+      registrar
+        .connect(addr3)
+        .transferFrom(addr1.address, addr3.address, sha3('newname')),
+    ).to.be.reverted;
+  });
 });
